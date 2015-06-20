72189ba562fbf55f4bcc8fdf93ffc59f069b27ed
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-20 20:27:09 +0900

    Extract shared procedure to before function

--- test/node-sass-cache-busted-url-test.js
+++ test/node-sass-cache-busted-url-test.js
@@ -7,33 +7,37 @@ var sass = require("node-sass");
 var cacheBustedUrl = require("../index");
 var cacheBusters = require("../cache-busters.json");
 
-it("sync", function() {
-  var sourceFile = __dirname + "/example1.sass";
-  var expected = fs.readFileSync(__dirname + "/example1.css").toString();
-  var actual = sass.renderSync({
-    file: sourceFile,
-    functions: {
-      "cache-busted-url($url)": cacheBustedUrl.bind(null, cacheBusters)
-    }
-  }).css.toString();
-  assert(expected === actual);
-});
+describe("cache-busted-url", function() {
+  before(function() {
+    this.sourceFile = __dirname + "/example1.sass";
+    this.expected = fs.readFileSync(__dirname + "/example1.css").toString();
+  });
 
-it("async", function(done) {
-  var sourceFile = __dirname + "/example1.sass";
-  var expected = fs.readFileSync(__dirname + "/example1.css").toString();
-  var actual = sass.render({
-    file: sourceFile,
-    functions: {
-      "cache-busted-url($url)": cacheBustedUrl.bind(null, cacheBusters)
-    }
-  }, function(err, result) {
-    if (err) {
-      assert.fail(err);
-      return;
-    };
-    var actual = result.css.toString();
-    assert(expected === actual);
-    done();
+  it("sync", function() {
+    var actual = sass.renderSync({
+      file: this.sourceFile,
+      functions: {
+        "cache-busted-url($url)": cacheBustedUrl.bind(null, cacheBusters)
+      }
+    }).css.toString();
+    assert(this.expected === actual);
+  });
+
+  it("async", function(done) {
+    var context = this;
+    var actual = sass.render({
+      file: this.sourceFile,
+      functions: {
+        "cache-busted-url($url)": cacheBustedUrl.bind(null, cacheBusters)
+      }
+    }, function(err, result) {
+      if (err) {
+        assert.fail(err);
+        return;
+      };
+      var actual = result.css.toString();
+      assert(context.expected === actual);
+      done();
+    });
   });
 });
