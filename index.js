3c1ef1f290d0f44938c4796cf3429521f99898bb
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-20 20:19:26 +0900

    Make it possible to call asynchronously

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var sass = require("node-sass");
 
-function cacheBustedUrl(cacheBusters, url) {
+function cacheBustedUrl(cacheBusters, url, done) {
   var value = url.getValue();
   if (! (value in cacheBusters)) {
     throw new Error("Cache buster for " + value + " not found");
@@ -10,7 +10,11 @@ function cacheBustedUrl(cacheBusters, url) {
   var newValue = cacheBusters[value];
   var replacement = sass.types.String('url("' + newValue + '")');
 
-  return replacement;
+  if (typeof done === "function") {
+    done(replacement);
+  } else {
+    return replacement;
+  }
 }
 
 module.exports = cacheBustedUrl;
--- test/node-sass-cache-busted-url-test.js
+++ test/node-sass-cache-busted-url-test.js
@@ -7,7 +7,7 @@ var sass = require("node-sass");
 var cacheBustedUrl = require("../index");
 var cacheBusters = require("../cache-busters.json");
 
-it("", function() {
+it("sync", function() {
   var sourceFile = __dirname + "/example1.sass";
   var expected = fs.readFileSync(__dirname + "/example1.css").toString();
   var actual = sass.renderSync({
@@ -17,4 +17,23 @@ it("", function() {
     }
   }).css.toString();
   assert(expected === actual);
+});
+
+it("async", function(done) {
+  var sourceFile = __dirname + "/example1.sass";
+  var expected = fs.readFileSync(__dirname + "/example1.css").toString();
+  var actual = sass.render({
+    file: sourceFile,
+    functions: {
+      "cache-busted-url($url)": cacheBustedUrl.bind(null, cacheBusters)
+    }
+  }, function(err, result) {
+    if (err) {
+      assert.fail(err);
+      return;
+    };
+    var actual = result.css.toString();
+    assert(expected === actual);
+    done();
+  });
 });
